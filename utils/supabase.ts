2690a2eafd435cd922638f726494862ce7675b39
zentag <38821230+zentag@users.noreply.github.com>
2025-08-08 14:31:08 -0400

    actually add the api key fr this time and not sum dum variable crap

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://nbqtgwywogiavgkuchef.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5icXRnd3l3b2dpYXZna3VjaGVmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ2NzUyNTAsImV4cCI6MjA3MDI1MTI1MH0.c-XIH4SoapzayrjKjiAZ9bM7YfsWhLMBeFPWhty7ah0"
 export const supabase = createClient(supabaseUrl, supabaseKey)
